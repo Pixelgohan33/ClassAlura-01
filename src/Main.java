cccbcfb188de173856dec9cedb951a29c951ddbb
Pixelgohan33 <pixelnon127gmail.com>
2024-05-02 12:05:59 -0600

    Screen01-Alura

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //fecha de lanzamiento
+        int fdl = 2014;
+        double evaluacion=2;
+        //Incluido en el plan basico
+        boolean InclPB=true;
+        double mediaeval = (1 + 5 + 3) / 3;
+        double mediaflash = 0;
+        String nombre= "The flash";
+        String Sinopsis= """
+                         
+                         """;
+        System.out.println("Titulo: " + nombre);
+        System.out.println("Fecha de lanzamiento: " + fdl);
+        System.out.println("Calificacion: " + evaluacion);
+        System.out.println("Empezar a ver: " + InclPB);
+        System.out.println("La evaluacion media de la pelicula " + nombre + "es: " + mediaeval);
+
+        if (fdl >= 2019) {
+            System.out.println("La mejor serie del momento");
+        } else{
+            System.out.println("Serie retro popular del momento");
+        }
+        int count = 0 ;
+        while (count < 3 ) {
+            count++;
+            Scanner teclado = new Scanner(System.in);
+            System.out.println("Ingresa la calificacion que le das a " + nombre);
+            double calflash = teclado.nextDouble();
+
+            mediaflash = mediaflash + calflash;
+        }
+        System.out.println("La media de la calificacion es: " + mediaflash / 3);
+    }
+}
